4bfd5c6afe27117a00078b3dd86299ee7fbd1d74
adminayushmandal <ayushmandal@mandalarc.net>
2026-04-16 15:09:08 +0530

    setup the AppHost Add postgress to the project.

--- ECommerce.AppHost/AppHost.cs
+++ ECommerce.AppHost/AppHost.cs
@@ -1,5 +1,17 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.ECommerce_Server>("ecommerce-server");
+var postgres = builder.AddPostgres("postgres")
+    .WithHostPort(54332)
+    .WithLifetime(ContainerLifetime.Persistent)
+    .WithDataVolume("ECommerce.Postgres.Data");
+
+string databaseName = "EcommerceDb";
+
+var database = postgres.AddDatabase(databaseName);
+
+builder.AddProject<Projects.ECommerce_Server>("api")
+    .WithHttpsEndpoint(5001, name: "api")
+    .WithReference(database)
+    .WaitFor(database);
 
 builder.Build().Run();
